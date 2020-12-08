a5430a9b7406ea5dd8c30b62f3c0dc4b6c1a875c
fraspi <==>
2020-12-08 22:21:40 +0300

    test.txt düzenlendi

--- test.py
+++ test.py
@@ -1,4 +1,28 @@
 import os
+import time
 
-os.system('git')
-os.system('code ..')
+while True:
+    girdi = input('Güncellemek istiyor musunuz? E/H :')
+
+    if girdi == 'E':
+        print('------------------- Güncelleme başlatılıyor -------------------')
+        time.sleep(1)
+        os.system('git status')
+        print('---------------------------------------------------------------')
+        os.system('git add .')
+        print('Dosyalar yerel depoya eklendi.')
+        print('---------------------------------------------------------------')
+        os.system('git status')
+        time.sleep(1)
+        print('Dosyalar uzak depoya gönderiliyor...')
+        os.system('git push')
+        print('Dosyalar uzak depoya gönderildi...')
+        print('Güncelleme başarılı.')
+        print('---------------------------------------------------------------')
+        break
+
+    else:
+        print('Güncelleme iptal edildi...')
+        time.sleep(1)
+
+#os.system('git')
