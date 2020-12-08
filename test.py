1f3a45db52aaac6e7c8f292d7d19d77364c3a128
fraspi <==>
2020-12-08 23:37:40 +0300

    güncelleme

--- test.py
+++ test.py
@@ -1,29 +1,22 @@
 import os
 import time
 
+mesaj = '''
+Güncelleme almak istiyor musunuz? e/h:
+Çıkmak için 'c'
+---------------------------------------------------------------
+Komut: 
+
+'''
 while True:
-    girdi = input('Güncellemek istiyor musunuz? E/H :')
 
-    if girdi == 'E':
-        print('------------------- Güncelleme başlatılıyor -------------------')
-        time.sleep(1)
-        os.system('git status')
-        print('---------------------------------------------------------------')
-        os.system('git add .')
-        os.system('git commit -m "test.txt düzenlendi"')
-        print('Dosyalar yerel depoya eklendi.')
-        print('---------------------------------------------------------------')
-        os.system('git status')
-        time.sleep(1)
-        print('Dosyalar uzak depoya gönderiliyor...')
-        os.system('git push')
-        print('Dosyalar uzak depoya gönderildi...')
-        print('Güncelleme başarılı.')
-        print('---------------------------------------------------------------')
-        break
-
-    else:
-        print('Güncelleme iptal edildi...')
-        time.sleep(1)
 
 #os.system('git')
+
+    def test():
+        os.system('git fetch')
+        my_file = open('version.txt', 'r')
+        version = my_file.read()
+
+        print(version)
+
