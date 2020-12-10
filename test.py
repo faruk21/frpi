9aae90a32367208b1bb9407979e9134b017065bf
fraspi <==>
2020-12-10 22:02:34 +0300

    .

--- test.py
+++ test.py
@@ -2,18 +2,53 @@ import os
 import time
 
 mesaj = '''
-Güncelleme almak istiyor musunuz? e/h:
-Çıkmak için 'c'
 ---------------------------------------------------------------
-Komut: 
+              Güncelleme kontrol sistemi
+---------------------------------------------------------------
+'''
+def old_vers_r():
+    old = open('local_version.txt', 'r')
+    eski_vers = old.read()
+    old.close()
+    print('-----------------------------------------------------------------')
+    print(f'Eski version: {eski_vers}')
+    print('-----------------------------------------------------------------')
+
+def old_vers_w(newv):
+    localv = open('local_version.txt', 'w')           # local_version.txt dosyası yazma modunda açıldı.
+    localv.write(newv)                                # newv'ye gelen veri local_version.txt dosyasına yazıldı.
+
+def update_control():
+    old_vers_r()
+    print('Güncelleme kontrol ediliyor.')
+    os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/remote_version.txt')
+
+    with open('remote_version.txt', 'r') as rm_file:  # İndirilen remote_version.txt yazma modunda açıldı.
+        rm_vrsn = rm_file.read()                      # remote_version.txt okundu rm_vrsn ye kaydedildi.
+    print('---------------------------------------------------------------')
+    print(f'Yeni version: {rm_vrsn}')                 
+    old_vers_w(rm_vrsn)                               # rm_vrsn old_vers'e gönderildi.
+    os.system('rm remote_version.txt')                # remote_version.txt'silindi.
+    print('---------------------------------------------------------------')    
 
 '''
-print('güncelleme kontrol dosyası alınıyor')
-os.system('git fetch')
-os.system('git pull')
 
-#my_file = open('version.txt', 'r')
-#version = my_file.read()
+    os.system('git fetch')
+    os.system('git pull')
+    my_file = open('version.txt', 'r')
 
-#print(version)
+def hesap():
+    sonuc = time.time() - starttime
+    print(sonuc)
+    #print('hesap çalışıyor')
+
+    if sonuc > 5:
+        print('işlkjhgf')
+        sonuc = time.time()
+        güncelleme(old)
+'''
+
+while True:
+    update_control()  
+    break
 
