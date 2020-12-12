2eb0f54a043a33a4d453b0b5b56cc0a77cd452ce
fraspi <==>
2020-12-12 17:50:37 +0300

    güncelleme

--- test.py
+++ test.py
@@ -6,29 +6,39 @@ mesaj = '''
               Güncelleme kontrol sistemi
 ---------------------------------------------------------------
 '''
-def old_vers_r():
+def remote_r():
+    remote = open('remote_version.txt', 'r')
+    remote_vers = remote.read() 
+    return remote_vers
+
+def localv_r():
     old = open('local_version.txt', 'r')
-    eski_vers = old.read()
-    old.close()
-    print('-----------------------------------------------------------------')
-    print(f'Eski version: {eski_vers}')
-    print('-----------------------------------------------------------------')
+    local_vers = old.read()
+    return local_vers
 
-def old_vers_w(newv):
+def localv_w(newv):
     localv = open('local_version.txt', 'w')           # local_version.txt dosyası yazma modunda açıldı.
-    localv.write(newv)                                # newv'ye gelen veri local_version.txt dosyasına yazıldı.
+    localv.write(newv)
 
-def update_control():
-    old_vers_r()
-    print('Güncelleme kontrol ediliyor.')
+#-------------------------------------------------------------------------------------------------------
+
+def update_control():                    
+    print('Güncelleme kontrol dosyası indiriliyor.')
+    print('-----------------------------------------------------------------')
     os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/remote_version.txt')
-
-    with open('remote_version.txt', 'r') as rm_file:  # İndirilen remote_version.txt yazma modunda açıldı.
-        rm_vrsn = rm_file.read()                      # remote_version.txt okundu rm_vrsn ye kaydedildi.
+    print('-----------------------------------------------------------------')
+    print('Güncelleme kontrol dosyası indirildi.')
+    print('Güncelleme kontrol dosyası okunuyor.')
     print('---------------------------------------------------------------')
-    print(f'Uzak depo version: {rm_vrsn}')
-    
-    os.system('rm remote_version.txt')                # remote_version.txt'silindi.
+    remoteread = remote_r()
+    localread = localv_r()
+    print(f'Local sürüm: {localread}')
+    print(f'serverdaki sürüm: {remoteread}')
+    print('---------------------------------------------------------------')
+    print('Local sürüm güncelleniyor')
+    #localv_w(remoteread)
+    print(f'Yeni local sürüm: {remoteread}')
+    #os.system('rm remote_version.txt')                # remote_version.txt'silindi.
     print('remote_version.txt silindi')
     print('---------------------------------------------------------------')  
 
