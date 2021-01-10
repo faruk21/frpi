515142eb64a21e39ff9db2c62eba1d11b8680ff4
root <root@DESKTOP-BADA9U6.localdomain>
2021-01-10 21:10:01 +0300

    pi

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import os
 import json
 import os.path
 
+print(os.path.exists("/root/Updater/frpi/version/remote_version.json"))
+
 remote_file = "remote_version.json"
 yol = "/root/Updater/version"
 
 path = os.path.join(yol, remote_file)
 #os.remove(path)
-os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/remote_version.json -P /root/Updater/version/')
+#os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/remote_version.json -P /root/Updater/version/')
--- updater.py
+++ updater.py
@@ -43,17 +43,17 @@ class perpetualTimer():
       self.thread.cancel()
 
 remote_file = "remote_version.json"
-yol = "/root/Updater/version"
+yol = "/root/Updater/frpi/version"
 path = os.path.join(yol, remote_file)
 
 def update():
     print(mesaj4)
     for x in range(1):           # Bu işlemi 3 kez dene.
         try:
-            #os.chdir("frpi")    #frpi dizinine geçildi.
-            #os.remove(path)
-            os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/remote_version.json -P /root/Updater/version/')
-            with open('remote_version.json', 'r') as f:
+            os.remove(path)
+            os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/version/remote_version.json -P /root/Updater/frpi/version/')
+            
+            with open('/root/Updater/frpi/version/remote_version.json', 'r') as f:
                 remote_v = json.load(f)
                 r_veriler = remote_v['versions']
                 r_version = r_veriler[0]['version_number']
@@ -61,10 +61,11 @@ def update():
             #r_ay = veriler[0]['month']
             #r_yıl = veriler[0]['year']
             #print(mesaj3)   # Uzak depoya  başarıyla erişildi Güncelleme kontrol dosyası indirildi.
-
+            
+            
             l_file_cntrl()
 
-            with open('local_version.json', 'r') as f:
+            with open('/root/Updater/frpi/version/local_version.json', 'r') as f:
                 local_v = json.load(f)
                 l_veriler = local_v['versions']
                 l_version = l_veriler[0]['version_number']
@@ -74,7 +75,7 @@ def update():
                 with open('remote_version.json', 'r') as f, open('local_version.json', 'w') as f2:
                     veri = json.load(f)
                     json.dump(veri, f2, indent=4)
-                with open ('local_version.json', 'r') as f2:
+                with open ('/root/Updater/frpi/version/local_version.json', 'r') as f2:
                     local_v = json.load(f2)
                     l_veriler = local_v['versions']
                     l_version = l_veriler[0]['version_number']
@@ -104,7 +105,7 @@ def l_file_cntrl():
     print('Lokal dosya kontrol ediliyor...')
     print('-----------------------------------------------------------------')
     try:
-        with open('local_version.json', 'r') as f:
+        with open('/root/Updater/frpi/version/local_version.json', 'r') as f:
             local_v = json.load(f)
             l_veriler = local_v['versions']
             l_version = l_veriler[0]['version_number']
@@ -113,7 +114,7 @@ def l_file_cntrl():
     except FileNotFoundError:
         #print('-----------------------------------------------------------------')
         #print('local file dosyası bulunamadı, yeniden oluşturuluyor...')
-        with open('remote_version.json', 'r') as f, open('local_version.json', 'w') as f2:
+        with open('/root/Updater/frpi/version/remote_version.json', 'r') as f, open('/root/Updater/frpi/version/local_version.json', 'w') as f2:
             veri = json.load(f)
             json.dump(veri, f2, indent=4)
             print("Locaf file tekrar oluşturuldu")
