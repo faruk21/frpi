5f683f50fe170731f804144cc643b00e312aa60a
faruk21 <faruk.343.1998@gmil.com>
2021-01-11 20:54:46 +0300

    a

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import os
 import json
 import os.path
 import time
+import requests
 
 remote_yolu = "./storage/shared/Python/Updater/frpi/version/remote_version.json"
 
@@ -19,11 +20,23 @@ path = os.path.join(yol, remote_file)
 #os.remove(path)
 '''
 #os.chdir('./storage/shared/Python/Updater/frpi/version')
-#os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/version/remote_version.json -P ./storage/shared/Python/Updater/frpi/version/')
+#os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/version/remote_version.json -P ./storage/shared/Python/Updater/frpi/version')
 #os.system("ls")
 
 
 #os.remove("Updater/frpi/version/remote_version.json")
+def check_internet():
+    url='http://www.google.com/'
+    timeout=5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        print("İnternet bağlantısı yok.")
+    return False
+    
+test = check_internet()
+print(test)
 '''
 git_add = 'git add .'
 git_commit = "git commit -m 'a' "
@@ -36,4 +49,37 @@ os.system(git_add)
 os.system(git_commit)
 os.system(git_push)
 os.system('cd')
+
+
+
+                with open(l_path, 'r') as f:
+                    local_v = json.load(f)
+                    l_veriler = local_v['versions']
+                    l_version = l_veriler[0]['version_number']
+    
+                if round(r_version,1) > l_version:
+                    print(f'güncelleme mevcut local: {l_version} remote: {round(r_version,1)}')
+                    with open(r_path, 'r') as f, open(l_path, 'w') as f2:
+                        veri = json.load(f)
+                        json.dump(veri, f2, indent=4)
+                    with open (l_path, 'r') as f2:
+                        local_v = json.load(f2)
+                        l_veriler = local_v['versions']
+                        l_version = l_veriler[0]['version_number']
+
+# git pull origin master
+# remote dan kodlarınızı local ortamınıza çekersiniz, default kendiliğinden merge işlemini yapar.
+# pull a benzer şekilde fetch  remote daki kodların kopyasını local e oluşturur, ancak  merge yapmaz.
+# 18:10
+                
+                os.chdir('./storage/shared/Python/Updater/frpi')
+                os.system(git_add)
+                os.system(git_commit)
+                os.system(git_pull)
+                os.system('cd')
+                print('---------------------------------------------------')
+                print(f"Güncelleme tamamlandı, güncel version: {l_version}")
+
+                else:
+                    print("Program zaten güncel")
 '''
--- updater.py
+++ updater.py
@@ -61,63 +61,18 @@ remote_yolu = "./storage/shared/Python/Updater/frpi/version/remote_version.json"
 
 def update():
     print(mesaj4)
-    for x in range(1):           # Bu işlemi 3 kez dene.
+    for x in range(2):
         if os.path.exists(remote_yolu):
-            #os.remove(r_path)
+            os.remove(remote_yolu)
             print("dosya silindi")
+        else:
+            print("Eski remote dosyası yok")
+        
         try:
-            os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/version/remote_version.json -P ./Updater/frpi/version/')
-            os.system('cd')
+            os.system('wget https://raw.githubusercontent.com/faruk21/frpi/main/version/remote_version.json -P ./storage/shared/Python/Updater/frpi/version')
             
-            with open(r_path, 'r') as f:
-                remote_v = json.load(f)
-                r_veriler = remote_v['versions']
-                r_version = r_veriler[0]['version_number']
-            #r_gün = veriler[0]['day']
-            #r_ay = veriler[0]['month']
-            #r_yıl = veriler[0]['year']
-            #print(mesaj3)   # Uzak depoya  başarıyla erişildi Güncelleme kontrol dosyası indirildi
-
-            l_file_cntrl()
-
-            with open(l_path, 'r') as f:
-                local_v = json.load(f)
-                l_veriler = local_v['versions']
-                l_version = l_veriler[0]['version_number']
-
-            if round(r_version,1) > l_version:
-                print(f'güncelleme mevcut local: {l_version} remote: {round(r_version,1)}')
-                with open(r_path, 'r') as f, open(l_path, 'w') as f2:
-                    veri = json.load(f)
-                    json.dump(veri, f2, indent=4)
-                with open (l_path, 'r') as f2:
-                    local_v = json.load(f2)
-                    l_veriler = local_v['versions']
-                    l_version = l_veriler[0]['version_number']
-
-# git pull origin master
-# remote dan kodlarınızı local ortamınıza çekersiniz, default kendiliğinden merge işlemini yapar.
-# pull a benzer şekilde fetch  remote daki kodların kopyasını local e oluşturur, ancak  merge yapmaz.
-# 18:10
-                
-                os.chdir('./storage/shared/Python/Updater/frpi')
-                os.system(git_add)
-                os.system(git_commit)
-                os.system(git_pull)
-                os.system('cd')
-                print('---------------------------------------------------')
-                print(f"Güncelleme tamamlandı, güncel version: {l_version}")
-
-            else:
-                print("Program zaten güncel")
-
         except FileNotFoundError:
-            #print('-----------------------------------------------------------------')
-            if x == 2:
-                print(mesaj2,x)
-            #print('internet veya dosya sorunu')
-        
-        
+            print("indirilemedi")
 
 def l_file_cntrl():
     print('-----------------------------------------------------------------')
@@ -142,9 +97,7 @@ def l_file_cntrl():
 
 #-------------------------------------------------------------------------------------------------------
 bir_dakika = 60
-saniye = 10
+saniye = 2
 t = perpetualTimer(saniye,update)
 print(mesaj)
 t.start()
-
-#
